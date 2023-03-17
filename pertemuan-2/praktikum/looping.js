6b3f639f0c567625b78f09fedd92ba5fb3cc66f7
Ahmad Huda Salam <huda@pop-os.localdomain>
2023-03-17 10:03:50 +0700

    Memindahkan folder ke branch

--- pertemuan-2/praktikum/looping.js
+++
@@ -1,4 +0,0 @@
-// Looping for
-for (let i = 1; i < 10; i++) {
-    console.log(`Looping ke: ${i}`);
-}
--- pertemuan-3/praktikum/destructing/destructing-array.js
+++
@@ -1,5 +0,0 @@
-const family = [ "Ahmad", "Huda", "Salam" ];
-
-const [suami, istri, anak] = family;
-
-console.log(suami, istri, anak);
