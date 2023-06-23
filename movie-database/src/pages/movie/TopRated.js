654f4810b704bd4f3e8160b20518e0f5db5835b4
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2023-06-23 09:32:30 +0700

    14

--- movie-database/src/pages/movie/TopRated.js
+++ movie-database/src/pages/movie/TopRated.js
@@ -3,13 +3,15 @@ import axios from 'axios'
 import ENDPOINTS from '../../utils/constants/endpoints'
 import Hero from '../../components/Hero/Hero'
 import Movies from '../../components/Movies/Movies'
+import { useDispatch } from 'react-redux'
+import { updateMovies } from '../../features/moviesSlice'
 
 export default function TopRatedMovies() {
-  const [movie, setMovie] = useState('')
+  const dispatch = useDispatch()
 
   async function getTopRatedMovie() {
     const response = await axios(ENDPOINTS.TOP_RATED)
-    setMovie(response.data.results)
+    dispatch(updateMovies(response.data.results))
   }
 
   useEffect(() => {
@@ -19,7 +21,7 @@ export default function TopRatedMovies() {
   return (
     <>
       <Hero />
-      <Movies movies={movie} title="Top Rated Movie" />
+      <Movies title="Top Rated Movie" />
     </>
   )
 }
